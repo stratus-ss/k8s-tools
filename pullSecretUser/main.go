6d8679f4414c5449eaf7993666223bd907a5d25c
stratus-ss <steve.ovens@x86innovations.com>
2022-03-24 09:37:59 -0500

    pullSecretUser -> updated variables, added cli flag and removed debug code

--- pullSecretUser/main.go
+++ pullSecretUser/main.go
@@ -20,6 +20,7 @@ func main() {
 	// Get the command line arguments from the user
 	serviceAccountName := flag.String("service-account", "deployer", "The name of the service account to find.")
 	kubeConfigFile := flag.String("kube-config", "", "Full path to kubeconfig")
+	dataType := flag.String("data-type", "dockerconfigjson", "The heading of the in the 'data' section of the secret you wish to inspect")
 	flag.Parse()
 
 	// If no kubeconfig is passed in, attempt to find it in a default location
@@ -37,35 +38,33 @@ func main() {
 	namespaces, _ := client.CoreV1().Namespaces().List(context.TODO(), metav1.ListOptions{})
 
 	for _, projectInfo := range namespaces.Items {
-		// Skip over the openshift projects by default
+		// get all the secrets in the current namespace
 		all_secrets, _ := client.CoreV1().Secrets(projectInfo.Name).List(context.TODO(), metav1.ListOptions{})
 		for _, secretsInfo := range all_secrets.Items {
-			if projectInfo.Name != "default" {
-				individual_secret, _ := client.CoreV1().Secrets(projectInfo.Name).Get(context.TODO(), secretsInfo.Name, metav1.GetOptions{})
-				for secretsKey, secretValue := range individual_secret.Data {
-					if strings.Contains(secretsKey, "dockerconfigjson") {
-						var result map[string]interface{}
-						json.Unmarshal([]byte(secretValue), &result)
-						// json structure {"auths":{"<repo>":{"username":"faker","password":"snoogy","email":"admin@me.com","auth":"ZmF2d5"}}}
-						auths := result["auths"].(map[string]interface{})
-						for _, val := range auths {
-							unknownRepo := val.(map[string]interface{})
-							var foundUsername string
-							var password string
-							for authHeadings, authValues := range unknownRepo {
-								if strings.Contains(authHeadings, "username") {
-									unknwonUsername := fmt.Sprintf("%v", authValues)
-									if unknwonUsername == *serviceAccountName {
-										foundUsername = unknwonUsername
-									}
-								}
-								if strings.Contains(authHeadings, "password") {
-									password = fmt.Sprintf("%v", authValues)
+			individual_secret, _ := client.CoreV1().Secrets(projectInfo.Name).Get(context.TODO(), secretsInfo.Name, metav1.GetOptions{})
+			for secretsKey, secretValue := range individual_secret.Data {
+				if strings.Contains(secretsKey, *dataType) {
+					var result map[string]interface{}
+					json.Unmarshal([]byte(secretValue), &result)
+					// json structure {"auths":{"<repo>":{"username":"faker","password":"snoogy","email":"admin@me.com","auth":"ZmF2d5"}}}
+					auths := result["auths"].(map[string]interface{})
+					for _, val := range auths {
+						unknownRepo := val.(map[string]interface{})
+						var foundUsername string
+						var password string
+						for authHeadings, authValues := range unknownRepo {
+							if strings.Contains(authHeadings, "username") {
+								unknwonUsername := fmt.Sprintf("%v", authValues)
+								if unknwonUsername == *serviceAccountName {
+									foundUsername = unknwonUsername
 								}
 							}
-							fmt.Printf("Project Name: %s \n   Secret Name: %s \n   Username: %s \n   Password %s\n\n", projectInfo.Name, secretsInfo.Name, foundUsername, password)
-
+							if strings.Contains(authHeadings, "password") {
+								password = fmt.Sprintf("%v", authValues)
+							}
 						}
+						fmt.Printf("Secret Name: %s \n   Project Name: %s \n   Username: %s \n   Password %s\n\n", secretsInfo.Name, projectInfo.Name, foundUsername, password)
+
 					}
 
 				}
