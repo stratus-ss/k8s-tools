21f81360f42851394a9bee79ab3b43c46f0049a6
stratus-ss <steve.ovens@x86innovations.com>
2022-03-25 07:26:53 -0500

    pullSecretUser --> added debug flag as well as try/catch block for better error handling/discovery

--- pullSecretUser/main.go
+++ pullSecretUser/main.go
@@ -12,6 +12,32 @@ import (
 	"k8s.io/client-go/tools/clientcmd"
 )
 
+type Block struct {
+	Try     func()
+	Catch   func(Exception)
+	Finally func()
+}
+
+type Exception interface{}
+
+func Throw(up Exception) {
+	panic(up)
+}
+
+func (tcf Block) Do() {
+	if tcf.Finally != nil {
+		defer tcf.Finally()
+	}
+	if tcf.Catch != nil {
+		defer func() {
+			if r := recover(); r != nil {
+				tcf.Catch(r)
+			}
+		}()
+	}
+	tcf.Try()
+}
+
 // This program will crawl the projects in a cluster and retrieve any secrets
 // which have the a specified username inside them (generally a pull secret)
 // this can be very slow as it introspects all of the secrets in the cluster
@@ -23,7 +49,10 @@ func main() {
 	firstDataType := flag.String("first-data-type", "dockerconfigjson", "The heading of the in the 'data' section of the secret you wish to inspect")
 	secondDataType := flag.String("second-data-type", "", "The heading of the in the 'data' section of the secret you wish to inspect")
 	ignoreOpenShiftProjects := flag.Bool("ignore-openshift", true, "Ignores the Openshift-* projects to speed things up")
+	debug := flag.Bool("debug", false, "Turns on some debug messages")
 	flag.Parse()
+
+	debugHeader := "\n(( DEBUG )) -->"
 
 	// If no kubeconfig is passed in, attempt to find it in a default location
 	if *kubeConfigFile == "" {
@@ -39,14 +68,21 @@ func main() {
 	client, _ := kubernetes.NewForConfig(config)
 	// get all the namespaces so that we can loop over the secrets in that project
 	namespaces, _ := client.CoreV1().Namespaces().List(context.TODO(), metav1.ListOptions{})
+	auths := make(map[string]interface{})
 
 	for _, projectInfo := range namespaces.Items {
 		// get all the secrets in the current namespace
+		if *debug != false {
+			fmt.Printf("%s Project is: %s", debugHeader, projectInfo.Name)
+		}
 		if *ignoreOpenShiftProjects == true && strings.Contains(projectInfo.Name, "openshift") {
 			continue
 		}
 		all_secrets, _ := client.CoreV1().Secrets(projectInfo.Name).List(context.TODO(), metav1.ListOptions{})
 		for _, secretsInfo := range all_secrets.Items {
+			if *debug != false {
+				fmt.Printf("%s      Secret is: %s", debugHeader, secretsInfo.Name)
+			}
 			individual_secret, _ := client.CoreV1().Secrets(projectInfo.Name).Get(context.TODO(), secretsInfo.Name, metav1.GetOptions{})
 
 			for secretsKey, secretValue := range individual_secret.Data {
@@ -55,32 +91,51 @@ func main() {
 					json.Unmarshal([]byte(secretValue), &result)
 					// json structure {"auths":{"<repo>":{"username":"faker","password":"snoogy","email":"admin@me.com","auth":"ZmF2d5"}}}
 					// Some maps may be empty, we want to ignore them as they wont have the keys we are looking for
-					if len(result) == 0 {
-
-					} else {
-						auths := result["auths"].(map[string]interface{})
-
-						for _, val := range auths {
-							unknownRepo := val.(map[string]interface{})
-							var foundUsername string
-							var password string
-							for authHeadings, authValues := range unknownRepo {
-								if strings.Contains(authHeadings, "username") {
-									unknownUser := fmt.Sprintf("%v", authValues)
-									if strings.ToLower(unknownUser) == strings.ToLower(*serviceAccountName) {
-										foundUsername = unknownUser
-									}
-								}
-								if strings.Contains(authHeadings, "password") {
-									password = fmt.Sprintf("%v", authValues)
-								}
-
+					Block{
+						Try: func() {
+							auths = result["auths"].(map[string]interface{})
+						},
+						Catch: func(e Exception) {
+							if *debug != false {
+								fmt.Printf("%s   WARNING!!  %s   has unexpected format", debugHeader, secretsInfo.Name)
 							}
-							if len(foundUsername) != 0 {
-								fmt.Printf("Secret Name: %s \n   Project Name: %s \n   Username: %s \n   Password %s\n\n", secretsInfo.Name, projectInfo.Name, foundUsername, password)
+						},
+						Finally: func() {
+						},
+					}.Do()
+					for _, val := range auths {
+						unknownRepo := make(map[string]interface{})
+						Block{
+							Try: func() {
+								unknownRepo = val.(map[string]interface{})
+							},
+							Catch: func(e Exception) {
+								if *debug != false {
+									fmt.Printf("%s   WARNING!!  %s   has unexpected format", debugHeader, secretsInfo.Name)
+								}
+							},
+							Finally: func() {
+							},
+						}.Do()
+						//unknownRepo := val.(map[string]interface{})
+						var foundUsername string
+						var password string
+						for authHeadings, authValues := range unknownRepo {
+							if strings.Contains(authHeadings, "username") {
+								unknownUser := fmt.Sprintf("%v", authValues)
+								if strings.ToLower(unknownUser) == strings.ToLower(*serviceAccountName) {
+									foundUsername = unknownUser
+								}
+							}
+							if strings.Contains(authHeadings, "password") {
+								password = fmt.Sprintf("%v", authValues)
 							}
 
 						}
+						if len(foundUsername) != 0 {
+							fmt.Printf("Secret Name: %s \n   Project Name: %s \n   Username: %s \n   Password %s\n\n", secretsInfo.Name, projectInfo.Name, foundUsername, password)
+						}
+
 					}
 				}
 			}
