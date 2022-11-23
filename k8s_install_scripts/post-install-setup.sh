66be8cc96b8f4ea1f57e8ac7a3a03972b9700de8
stratus-ss <steve.ovens@x86innovations.com>
2022-11-23 17:36:39 -0600

    added some helper scripts for post-k8s installation related to labs https://github.com/stratus-ss/k8s-labs

---
+++ k8s_install_scripts/post-install-setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# This script is related to a single node installation of vanilla k8s
+# It is meant to be run after the kubeadm --init has been run
+
+# remove the taint preventing scheduling on the node 
+kubectl taint nodes --all node-role.kubernetes.io/control-plane-
+
+
+wget https://raw.githubusercontent.com/kubeovn/kube-ovn/release-1.10/dist/images/install.sh
+mv install.sh ovn-install.sh
+sh ovn-install.sh
+
+cat <<EOF | sudo tee haproxy-ingress-values.yaml
+controller:
+  hostNetwork: true
+EOF
+
+
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+sh get_helm.sh
+helm repo add haproxy-ingress https://haproxy-ingress.github.io/charts
+helm repo update
+helm install haproxy-ingress haproxy-ingress/haproxy-ingress  --create-namespace --namespace ingress-controller  --version 0.13.9  -f haproxy-ingress-values.yaml
+
